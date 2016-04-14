86578ea56a88c28bedd11ba886b13ba11e96f9dd
Yu Li <liyu001989@gmail.com>
2016-04-14 23:55:03 +0800

    add trans helper

--- app/helpers.php
+++ app/helpers.php
@@ -30,3 +30,22 @@ if (!function_exists('dingo_route')) {
             ->route($name, $params);
     }
 }
+
+if (!function_exists('trans')) {
+    /**
+     * Translate the given message.
+     *
+     * @param  string  $id
+     * @param  array   $parameters
+     * @param  string  $domain
+     * @param  string  $locale
+     * @return string
+     */
+    function trans($id = null, $parameters = [], $domain = 'messages', $locale = null)
+    {
+        if (is_null($id)) {
+            return app('translator');
+        }
+        return app('translator')->trans($id, $parameters, $domain, $locale);
+    }
+}
