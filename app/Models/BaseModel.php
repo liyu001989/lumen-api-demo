e872e692293e48437d90944e25230dc41ec85ec4
Yu Li <liyu001989@gmail.com>
2017-03-02 22:11:44 +0800

    fix baseModel

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -8,7 +8,7 @@ class BaseModel extends Model
 {
     protected $guarded = ['id', 'updated_at'];
 
-    protected $casts = ['created_at', 'updated_at'];
+    protected $dates = ['created_at', 'updated_at'];
 
     protected $hidden = ['updated_at', 'deleted_at', 'extra'];
 }
