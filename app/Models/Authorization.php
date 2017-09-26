5c676029898084c0dbf451e23c859925f0d2e339
Yu Li <liyu001989@gmail.com>
2017-09-26 13:38:14 +0800

    update authorization model

--- app/Models/Authorization.php
+++ app/Models/Authorization.php
@@ -8,6 +8,8 @@ class Authorization
 {
     protected $token;
 
+    protected $payload;
+
     public function __construct($token = null)
     {
         $this->token = $token;
@@ -20,27 +22,50 @@ class Authorization
         return $this;
     }
 
+    public function getToken()
+    {
+        if (! $this->token) {
+            throw new \Exception('请设置token');
+        }
+
+        return $this->token;
+    }
+
     public function getPayload()
     {
-        return \Auth::setToken($this->token)->getPayload();
+        if (! $this->payload) {
+            $this->payload = \JWTAuth::setToken($this->getToken())->getPayload();
+        }
+
+        return $this->payload;
+    }
+
+    public function getExpiredAt()
+    {
+        return Carbon::createFromTimestamp($this->getPayload()->get('exp'))
+            ->toDateTimeString();
+    }
+
+    public function getRefreshExpiredAt()
+    {
+        return Carbon::createFromTimestamp($this->getPayload()->get('iat'))
+            ->addMinutes(config('jwt.refresh_ttl'))
+            ->toDateTimeString();
+    }
+
+    public function user()
+    {
+        return \JWTAuth::authenticate($this->getToken());
     }
 
     public function toArray()
     {
-        $payload = $this->getPayload();
-        // get expired time
-        $expiredAt = Carbon::createFromTimestamp($payload->get('exp'))->toDateTimeString();
-        // refresh expired time
-        $refreshExpiredAt = Carbon::createFromTimestamp($payload->get('iat'))
-            ->addMinutes(config('jwt.refresh_ttl'))
-            ->toDateTimeString();
-
         return [
-            // maybe you want a id for jsonapi.org format
-            //'id' => hash('md5', $this->token),
-            'token' => $this->token,
-            'expired_at' => $expiredAt,
-            'refresh_expired_at' => $refreshExpiredAt,
+            // maybe you need a id when use jsonapi.org format
+            //'id' => hash('md5', $this->getToken()),
+            'token' => $this->getToken(),
+            'expired_at' => $this->getExpiredAt(),
+            'refresh_expired_at' => $this->getRefreshExpiredAt(),
         ];
     }
 }
