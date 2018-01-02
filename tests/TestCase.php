d47896638971523fb82ec7731e08b1ad18a58dae
yang <316193839@qq.com>
2018-01-02 13:07:02 +0800

    增加了单元测试部分代码

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,22 @@
 <?php
 
-class TestCase extends Laravel\Lumen\Testing\TestCase
+namespace Tests;
+
+use Laravel\Lumen\Testing\TestCase as Basic;
+use \Laravel\Lumen\Testing\DatabaseTransactions;
+
+class TestCase extends Basic
 {
+    use DatabaseTransactions;//回滚数据库数据 如果增删改操作测试不想对原数据产生影响可以引入这个
+    protected $header;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        //如果token过期则运行一次AuthorizationTest即可 平常测试可相应运行需要测试的单元
+        $this->header = ['Authorization'=>'bearer '.@file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'token.txt')];
+    }
+
     /**
      * Creates the application.
      *
@@ -9,6 +24,8 @@ class TestCase extends Laravel\Lumen\Testing\TestCase
      */
     public function createApplication()
     {
-        return require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
+        return $app;
     }
+
 }
---
+++ tests/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+class UserTest extends TestCase
+{
+
+    public function testRegister()
+    {
+        $this->post("/api/users",['email'=>'1234@gmail.com','name'=>'test','password'=>'123456'])->assertResponseStatus(201);
+    }
+
+    public function testUserShow()
+    {
+        $this->get('/api/user',$this->header)->assertResponseOk();
+    }
+}
