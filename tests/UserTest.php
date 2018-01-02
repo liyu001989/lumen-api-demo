5fd6731d578d1bd76a597ee9b0a8c4075fada946
yang <316193839@qq.com>
2018-01-02 13:23:05 +0800

    修正了代码格式

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,7 +6,7 @@ class UserTest extends TestCase
 {
     public function testRegister()
     {
-        $this->post("/api/users", ['email'=>'1234@gmail.com', 'name'=>'test','password'=>'123456'])->assertResponseStatus(201);
+        $this->post('/api/users', ['email'=>'1234@gmail.com', 'name'=>'test','password'=>'123456'])->assertResponseStatus(201);
     }
 
     public function testUserShow()
