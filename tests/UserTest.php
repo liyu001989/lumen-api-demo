a950edc15e9445e20dc23fb58e339251114920e6
yang <316193839@qq.com>
2018-03-06 13:10:00 +0800

    使用了lumen提供的工厂方法来模拟一个用户

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,11 +6,14 @@ class UserTest extends TestCase
 {
     public function testRegister()
     {
-        $this->post('/api/users', ['email'=>'1234@gmail.com', 'name'=>'test', 'password'=>'123456'])->assertResponseStatus(201);
+        $this->withoutEvents();
+        $this->withoutJobs();
+        $this->post('/api/users', ['email' => '1234@gmail.com', 'name' => 'test', 'password' => '123456'])->assertResponseStatus(201);
     }
 
     public function testUserShow()
     {
         $this->get('/api/user', $this->header)->assertResponseOk();
     }
+
 }
